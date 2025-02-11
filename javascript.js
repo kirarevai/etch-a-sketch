ab901c2151b3ed2af47a77adf62b572f561a86d1
Amer <163821732+kirarevai@users.noreply.github.com>
2025-02-10 22:17:08 -0500

    Added reset grid button

---
+++ javascript.js
@@ -0,0 +1,51 @@
+const newGrid = document.createElement("button");
+newGrid.textContent = "Reset grid"; 
+document.body.appendChild(newGrid);
+newGrid.classList.add("reset");
+
+newGrid.addEventListener("click", () => {
+    let size = prompt("Choose dimensions for your new grid (max 100)");
+
+    size = parseInt(size);
+
+    if (isNaN(size) || size < 1 || size  > 100) {
+        alert("Please choose a valid size between 1 and 100!");
+        return;
+    }
+
+    resetGrid(size);
+
+});
+
+function resetGrid(size) {
+    container.innerHTML = "";
+
+    for (let i = 0; i < size * size; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("square");
+        container.appendChild(grid);
+    
+        grid.addEventListener("mouseover", () => 
+            {grid.style.backgroundColor = "black";}
+        );    
+    };
+
+    document.querySelectorAll(".square").forEach(square => {
+        square.style.width = `${320 / size}px`;
+        square.style.height = `${320 / size}px`;
+    }) 
+    
+}
+
+const container = document.getElementById("container");
+
+for (let i = 0; i < 16 * 16; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("square");
+    container.appendChild(grid);
+
+    grid.addEventListener("mouseover", () => 
+        {grid.style.backgroundColor = "black";}
+    );    
+};
+
